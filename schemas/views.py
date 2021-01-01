759a07d23d84afe4675694d6f45dfed3283fd044
MrFlava <thatelitemaili33t@gmail.com>
2021-01-01 16:32:21 +0200

    Completed test version of the task

--- schemas/views.py
+++ schemas/views.py
@@ -138,28 +138,29 @@ def GenerateDataView(request, schema_id):
 
     fake = Faker()
     columns = Column.objects.filter(schema_id=schema_id)
-    column_rows = []
+    # column_rows = [{column.name: None for column in columns}]
+    column_rows = [{column.name: None for column in columns} for _ in range(10)]
 
     for column in columns:
-        for i in range(10):
+        for i in range(0, len(column_rows)):
             if column.type == 'PHONE_NUMBER':
-                column_rows.append({column.name: fake.phone_number()})
+                column_rows[i].update({column.name: fake.phone_number()})
             if column.type == 'FULL_NAME':
-                column_rows.append({column.name: fake.name()})
+                column_rows[i].update({column.name: fake.name()})
             if column.type == 'JOB':
-                column_rows.append({column.name: fake.job()})
+                column_rows[i].update({column.name: fake.job()})
             if column.type == 'EMAIL':
-                column_rows.append({column.name: fake.email()})
+                column_rows[i].update({column.name: fake.email()})
             if column.type == 'COMPANY_NAME':
-                column_rows.append({column.name: fake.company()})
+                column_rows[i].update({column.name: fake.company()})
             if column.type == 'TEXT':
-                column_rows.append({column.name: fake.text()})
+                column_rows[i].update({column.name: fake.text()})
             if column.type == 'INTEGER':
-                column_rows.append({column.name: random.randrange(column.from_range, column.to_range)})
+                column_rows[i].update({column.name: random.randrange(column.from_range, column.to_range)})
             if column.type == 'ADDRESS':
-                column_rows.append({column.name: fake.address()})
+                column_rows[i].update({column.name: fake.address()})
             if column.type == 'DATE':
-                column_rows.append({column.name: fake.date()})
+                column_rows[i].update({column.name: fake.date()})
     context = {}
 
     csv_file = "CSVproject_main/media/data.csv"
@@ -170,7 +171,6 @@ def GenerateDataView(request, schema_id):
             writer.writeheader()
             for data in column_rows:
                 writer.writerow(data)
-
     except IOError:
         print("I/O error")
 
