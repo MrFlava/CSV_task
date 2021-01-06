3d77d19ebdb1c31344d1d63998b14770d72d28b4
MrFlava <thatelitemaili33t@gmail.com>
2021-01-06 23:09:40 +0200

    Fixes

--- schemas/service.py
+++ schemas/service.py
@@ -10,15 +10,15 @@ def generate_csv_data(schema, rows):
     columns = Column.objects.filter(schema_id=schema)
     schema = Schema.objects.get(pk=schema)
     column_rows = [{column.name: None for column in columns} for _ in range(rows)]
-
+    column_names = []
     row_types_methods = {'PHONE_NUMBER': 'fake.phone_number()', 'FULL_NAME': 'fake.name()', 'JOB': 'fake.job()',
                          'EMAIL': 'fake.email()', 'COMPANY_NAME': 'fake.company()', 'TEXT': 'fake.text()',
                          'INTEGER': 'random.randrange(column.from_range, column.to_range)', 'ADDRESS': 'fake.address()',
                          'DATE': 'fake.date()'}
     column_separators = {'TAB':  '  ', 'SEMICOLON':  ';', 'COMMA':  ',', 'SPACE':  ' '}
     string_characters = {'DOUBLE_QUOTE': '"', 'COLONEL': "'"}
-
     for column in columns:
+        column_names.insert(column.order, column.name)
         for i in range(0, len(column_rows)):
                 column_rows[i].update({column.name: eval(row_types_methods[column.type])})
 
@@ -27,13 +27,14 @@ def generate_csv_data(schema, rows):
         with open(csv_file, 'w') as csvfile:
             writer = csv.DictWriter(csvfile,
                                     quoting=csv.QUOTE_ALL,
-                                    fieldnames=[column.name for column in columns],
+                                    fieldnames=column_names,
                                     delimiter=f"{column_separators[schema.column_separator]}",
                                     quotechar=string_characters[schema.string_character],
                                     escapechar="\\")
             writer.writeheader()
+
             for data in column_rows:
-                for key, value in data.items():
+                for key in data.keys():
                     data[key] = str(data[key]).replace(' ', f'{column_separators[schema.column_separator]}')
                     if string_characters[schema.string_character] is '"':
                         data[key] = f'"{data[key]}"'
